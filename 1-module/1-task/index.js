ace435399eee61b9487b996d1873fed6c7417383
Pavel Cherepanov <cherepanov.pavel@list.ru>
2022-09-29 13:30:22 +0500

    решение задачи 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+  let res = 1;
+
+  if (n === 0 || n === 1) {
+    return res;
+  }
+
+  for (let i = 1; i <= n; i++) {
+    res = res * i;
+  } 
+
+  return res;
 }
+

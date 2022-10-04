cbc424490025be7cfbabb118248886f09f44ced6
Pavel Cherepanov <cherepanov.pavel@list.ru>
2022-10-04 13:56:19 +0500

    поправил все решения

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -3,7 +3,7 @@ function print(text) {
 }
 
 function isValid(name) {
-<<<<<<< HEAD
+
   // ваш код...
   if (name !== null && !name.includes(' ') && name.length >= 4) {
     name = true;
@@ -12,10 +12,6 @@ function isValid(name) {
   } 
   return name;
 } 
-=======
-  return !!name && !name.includes(' ') && name.length >= 4;
-}
->>>>>>> ba50f449b754497aadb05498078dd1564aa30a36
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
